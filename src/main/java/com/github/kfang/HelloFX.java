c4cafde4f6491ff88ff9f10b93bae31f77f4b1e1
Kevin Fang <hkr.fang@gmail.com>
2019-10-31 21:54:10 -0700

    setup fxml loader

--- src/main/java/com/github/kfang/HelloFX.java
+++ src/main/java/com/github/kfang/HelloFX.java
@@ -1,21 +1,21 @@
 package com.github.kfang;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.List;
 
 public class HelloFX extends Application {
 
     @Override
-    public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/main.fxml"));
+
+        Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
 
