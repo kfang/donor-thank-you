3c26639aa9201dd01eb461a20afd35fabf4a26b6
Kevin Fang <hkr.fang@gmail.com>
2020-01-04 18:55:32 -0800

    generate pdfs instead

--- src/main/java/com/github/kfang/ApachePoi.java
+++ src/main/java/com/github/kfang/ApachePoi.java
@@ -1,13 +1,19 @@
 package com.github.kfang;
 
+import com.documents4j.api.DocumentType;
+import com.documents4j.api.IConverter;
+import com.documents4j.job.LocalConverter;
+import org.apache.pdfbox.io.MemoryUsageSetting;
+import org.apache.pdfbox.multipdf.PDFMergerUtility;
 import org.apache.poi.xwpf.usermodel.*;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ApachePoi {
 
-    private static void writeReceipt(XWPFDocument doc, SalesReceiptGroup group) throws IOException {
+    private static File writeReceipt(XWPFDocument doc, SalesReceiptGroup group) throws IOException {
         doc.getParagraphs().forEach((paragraph) -> {
             paragraph.getRuns().forEach((run) -> {
                 String text = run.text();
@@ -49,22 +55,51 @@ public class ApachePoi {
             }
         });
 
-        String filename = "./output/" + group.getName() + ".docx";
-        OutputStream os = new FileOutputStream(filename);
-        doc.write(os);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        doc.write(out);
+        ByteArrayInputStream in = new ByteArrayInputStream(out.toByteArray());
+        System.out.println("generated docx for " + group.getName());
+
+        String filename = "./output/" + group.getName() + ".pdf";
+        FileOutputStream os = new FileOutputStream(filename);
+        IConverter converter = LocalConverter.builder().build();
+        converter.convert(in).as(DocumentType.DOCX).to(os).as(DocumentType.PDF).execute();
+        os.close();
+
         System.out.println("wrote " + filename);
+        return new File(filename);
     }
 
     public static void writeReceipts(File templateFile, List<SalesReceiptGroup> groups) {
+        ArrayList<File> files = new ArrayList<File>();
+
         groups.forEach((group) -> {
             try {
                 InputStream templateIS = new FileInputStream(templateFile);
                 XWPFDocument template = new XWPFDocument(templateIS);
-                writeReceipt(template, group);
+                File f = writeReceipt(template, group);
+                files.add(f);
             } catch (IOException e) {
                 e.printStackTrace();
             }
         });
+
+        PDFMergerUtility ut = new PDFMergerUtility();
+        ut.setDestinationFileName("./output/__merged.pdf");
+
+        files.forEach((f) -> {
+            try {
+                ut.addSource(f);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        });
+
+        try {
+            ut.mergeDocuments(MemoryUsageSetting.setupTempFileOnly());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
